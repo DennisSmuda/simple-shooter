c75a3cf27f3ea58d0f0e5b0ab349224ae032834a
Dennis Smuda <dennissmuda@msn.com>
2016-07-10 19:34:29 +0200

    Initial Commit

---
+++ conf.lua
@@ -0,0 +1,10 @@
+
+-- Config
+function love.conf(t)
+  t.title = "Simple Shooter"
+  t.version = "0.10.1"
+  t.window.width = 480
+  t.window.height = 800
+
+
+end
---
+++ lib/player.lua
@@ -0,0 +1,16 @@
+
+player = {
+  x = windowWidth/2 - 32,
+  y = windowHeight - 100,
+  speed = 150,
+  width = 64,
+  height = 64,
+  img = nil,
+  sheet = nil
+}
+
+bullets = {}
+
+function player.health()
+
+end
